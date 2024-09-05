fc0da7404f3e371928757f50a83410c02c044dc5
art <art@gmail.com>
2024-09-05 14:58:07 +0300

    delete some log

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+cd course-settings
 git pull
+cd ..
+
 pip3 install -r course-settings/requirements.txt
 cp ./course-settings/clang-format.json .clang-format
 cp ./course-settings/clang-tidy.json .clang-tidy
 
 python3 course-settings/checker/main.py
+
